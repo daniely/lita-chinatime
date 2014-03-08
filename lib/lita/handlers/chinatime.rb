7312f69889482a0cbfee0a921699459a6d7b1427
Daniel <robotright@gmail.com>
2014-03-08 22:18:47 +0800

    implement

--- lib/lita/handlers/chinatime.rb
+++ lib/lita/handlers/chinatime.rb
@@ -1,6 +1,35 @@
+require 'date'
+
 module Lita
   module Handlers
     class Chinatime < Handler
+      route %r{^chinatime$}i, :chinatime, command: true, help: { 'chinatime' => 'display current time in China' }
+
+      def chinatime(response)
+        # 35 103 is loc of china
+        resp = http.get('http://www.earthtools.org/timezone/35/103')
+
+        raise 'Not found' if resp.status == 404
+
+        date_time = extract_date_time(resp)
+        response.reply format_date_time(DateTime.parse(date_time))
+      rescue
+        reponse.reply error
+      end
+
+      private
+
+      def extract_date_time(resp)
+        resp.body.match(/<localtime>(.*)<\/localtime>/)[1]
+      end
+
+      def format_date_time(date_time)
+        date_time.strftime('%e %b %Y%l:%m:%S %p')
+      end
+
+      def error
+        "Sorry, but there was a problem getting the time."
+      end
     end
 
     Lita.register_handler(Chinatime)
--- spec/lita/handlers/chinatime_spec.rb
+++ spec/lita/handlers/chinatime_spec.rb
@@ -1,4 +1,38 @@
 require "spec_helper"
 
 describe Lita::Handlers::Chinatime, lita_handler: true do
+  it { routes_command("chinatime").to(:chinatime) }
+
+  describe "#chinatime" do
+    let(:response) { double("Faraday::Response") }
+
+    before do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(response)
+    end
+
+    it "replies with an image URL on success" do
+      allow(response).to receive(:status).and_return(200)
+      allow(response).to receive(:body).and_return(<<-HTML.chomp
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+  <timezone xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.earthtools.org/timezone.xsd">
+  <version>1.0</version>
+  <location>
+    <latitude>35</latitude>
+    <longitude>103</longitude>
+  </location>
+  <offset>8</offset>
+  <suffix>H</suffix>
+  <localtime>8 Mar 2014 22:07:45</localtime>
+  <isotime>2014-03-08 22:07:45 +0800</isotime>
+  <utctime>2014-03-08 14:07:45</utctime>
+  <dst>Unknown</dst>
+</timezone>
+HTML
+        )
+
+      send_command('chinatime')
+
+      expect(replies.last).to eq(' 8 Mar 201410:03:45 PM')
+    end
+  end
 end
